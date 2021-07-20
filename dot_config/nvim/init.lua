3947e707406e266ae41c69377003616e4d82bb53
Julien Dambron <dreamlike3d@gmail.com>
2021-07-20 13:38:54 +0200

    feat(nvim): update config to Lua

---
+++ dot_config/nvim/init.lua
@@ -0,0 +1,17 @@
+-- plugins
+require 'paq' {
+    'savq/paq-nvim'; -- Let Paq manage itself
+    'tjdevries/colorbuddy.vim';
+    'tjdevries/gruvbuddy.nvim';
+    'jremmen/vim-ripgrep';
+    'justinmk/vim-dirvish';
+    'sheerun/vim-polyglot';
+    'tpope/vim-commentary';
+    'tpope/vim-fugitive';
+    'tpope/vim-surround';
+    'vim-airline/vim-airline';
+}
+-- options
+require 'settings' 
+-- mappings
+require 'mappings'
---
+++ dot_config/nvim/lua/mappings.lua
@@ -0,0 +1,2 @@
+-- Double esc to remove search highlights
+vim.api.nvim_set_keymap('n', '<esc><esc>', ':noh<return><esc>', {noremap = true, silent = true})
