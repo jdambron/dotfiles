fd206a029f60ee1a4f1420fee9501e31b7724b58
jdambron <jdambron@users.noreply.github.com>
2021-08-02 10:36:22 +0200

    feat(nvim): add a remap from Y to y$

--- dot_config/nvim/lua/mappings.lua
+++ dot_config/nvim/lua/mappings.lua
@@ -1,2 +1,4 @@
 -- Double esc to remove search highlights
 vim.api.nvim_set_keymap('n', '<esc><esc>', ':noh<return><esc>', {noremap = true, silent = true})
+-- Replace Y with y$
+vim.api.nvim_set_keymap('n', 'Y', 'y$', {noremap = true, silent = true})
