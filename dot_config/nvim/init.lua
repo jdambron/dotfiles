be9d0f357985d2068bb94658a0ca245bab5e7847
jdambron <dreamlike3d@gmail.com>
2021-12-03 10:25:44 +0100

    chore: replace paq-nvim with packer

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,17 +1,5 @@
 -- plugins
-require 'paq' {
-    'savq/paq-nvim'; -- Let Paq manage itself
-    'tjdevries/colorbuddy.vim';
-    'tjdevries/gruvbuddy.nvim';
-    'jremmen/vim-ripgrep';
-    'justinmk/vim-dirvish';
-    'sheerun/vim-polyglot';
-    'tpope/vim-commentary';
-    'tpope/vim-fugitive';
-    'tpope/vim-surround';
-    'nvim-lualine/lualine.nvim';
-    'kyazdani42/nvim-web-devicons';
-}
+require 'user.plugins'
 -- options
 require 'settings' 
 -- mappings
