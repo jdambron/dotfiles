714e446eeffd202056f547727b977a0e5a854e09
jdambron <dreamlike3d@gmail.com>
2022-12-02 13:52:37 +0100

    feat(nvim): add time and telescope keymaps

--- dot_config/nvim/lua/user/keymaps.lua
+++ dot_config/nvim/lua/user/keymaps.lua
@@ -11,6 +11,13 @@ keymap('n', 'Y', 'y$', opts)
 -- Move text up and down
 keymap("n", "<A-j>", "<Esc>:m .+1<CR>==gi", opts)
 keymap("n", "<A-k>", "<Esc>:m .-2<CR>==gi", opts)
+-- Insert date in ISO format
+keymap("n", "<F5>", "\"=strftime(\"%FT%T\")<CR>p", opts)
+-- Telescope mappings
+keymap("n", "<leader>ff", "<cmd>lua require('telescope.builtin').find_files()<cr>", opts)
+keymap("n", "<leader>fg", "<cmd>lua require('telescope.builtin').live_grep()<cr>", opts)
+keymap("n", "<leader>fb", "<cmd>lua require('telescope.builtin').buffers()<cr>", opts)
+keymap("n", "<leader>fh", "<cmd>lua require('telescope.builtin').help_tags()<cr>", opts)
 
 -- Visual --
 -- Stay in indent mode
@@ -27,3 +34,7 @@ keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
 keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
 keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
 keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
+
+-- Insert mode --
+-- Insert date in ISO format
+keymap("i", "<F5>", "<C-R>=strftime(\"%FT%T\")<CR>", opts)
