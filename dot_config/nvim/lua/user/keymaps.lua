61043401a8f4c62813d4c27fd7c49024558ae8b4
jdambron <dreamlike3d@gmail.com>
2022-12-19 10:55:26 +0100

    feat(nvim): improve telescope keymaps

--- dot_config/nvim/lua/user/keymaps.lua
+++ dot_config/nvim/lua/user/keymaps.lua
@@ -15,10 +15,15 @@ keymap("n", "<A-k>", "<Esc>:m .-2<CR>==gi", opts)
 -- Insert date in ISO format
 keymap("n", "<F5>", "\"=strftime(\"%FT%T\")<CR>p", opts)
 -- Telescope mappings
-keymap("n", "<leader>ff", "<cmd>lua require('telescope.builtin').find_files()<cr>", opts)
-keymap("n", "<leader>fg", "<cmd>lua require('telescope.builtin').live_grep()<cr>", opts)
-keymap("n", "<leader>fb", "<cmd>lua require('telescope.builtin').buffers()<cr>", opts)
-keymap("n", "<leader>fh", "<cmd>lua require('telescope.builtin').help_tags()<cr>", opts)
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>fs', function()
+  builtin.grep_string({ search  = vim.fn.input("Grep > ") })
+end)
+vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 
 -- Visual --
 -- Stay in indent mode
