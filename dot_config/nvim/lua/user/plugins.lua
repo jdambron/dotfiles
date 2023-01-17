89e36f5a5244632e3526310f1bcb81fad00e121b
jdambron <dreamlike3d@gmail.com>
2023-01-17 10:43:31 +0100

    feat(nvim): replace packer with lazy.nvim

--- dot_config/nvim/lua/user/plugins.lua
+++ dot_config/nvim/lua/user/plugins.lua
@@ -1,91 +1,63 @@
-local fn = vim.fn
-
--- Automatically install packer
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-if fn.empty(fn.glob(install_path)) > 0 then
-  PACKER_BOOTSTRAP = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-  print("Installing packer close and reopen Neovim...")
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
+vim.opt.rtp:prepend(lazypath)
 
--- Autocommand that reloads neovim whenever you save the plugins.lua file
-vim.cmd([[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerSync
-  augroup end
-]])
-
--- Use a protected call so we don't error out on first use
-local status_ok, packer = pcall(require, "packer")
-if not status_ok then
-  return
-end
-
--- Have packer use a popup window
-packer.init({
-    display = {
-      open_fn = function()
-        return require('packer.util').float({ border = 'single' })
-      end
-    }
-  }
-)
-
--- Install your plugins here
-return packer.startup(function(use)
+require('lazy').setup({
   -- My plugins here
-  use 'wbthomason/packer.nvim' -- Have packer manage itself
-  use 'jremmen/vim-ripgrep'
-  use 'justinmk/vim-dirvish'
-  use 'kyazdani42/nvim-web-devicons'
-  use {
+  'jremmen/vim-ripgrep',
+  'justinmk/vim-dirvish',
+  'kyazdani42/nvim-web-devicons',
+  {
     'lewis6991/gitsigns.nvim',
     config = function()
       require('gitsigns').setup()
     end
-  }
-  use {
+  },
+  {
     'numToStr/Comment.nvim',
     config = function()
         require('Comment').setup()
     end
-  }
-  use 'nvim-lualine/lualine.nvim'
-  use 'tpope/vim-fugitive'
-  use 'tpope/vim-sleuth'
-  use 'tpope/vim-surround'
-  use {
+  },
+  'nvim-lualine/lualine.nvim',
+  'tpope/vim-fugitive',
+  'tpope/vim-sleuth',
+  'tpope/vim-surround',
+  {
     'windwp/nvim-autopairs',
     config = function()
       require('nvim-autopairs').setup{}
     end
-  }
-  use { "catppuccin/nvim", as = "catppuccin" }
-  use {
+  },
+  { 'catppuccin/nvim', name = 'catppuccin' },
+  {
     'nvim-telescope/telescope.nvim', tag = '0.1.0',
     -- or                            , branch = '0.1.x',
-    requires = { {'nvim-lua/plenary.nvim'} }
-  }
-  use {
+    dependencies = { {'nvim-lua/plenary.nvim'} }
+  },
+  {
     'nvim-treesitter/nvim-treesitter',
-    run = ':TSUpdate',
-  }
-  use {
-    "folke/which-key.nvim",
+    build = ':TSUpdate',
+  },
+  {
+    'folke/which-key.nvim',
     config = function()
       vim.o.timeout = true
       vim.o.timeoutlen = 300
-      require("which-key").setup {
+      require('which-key').setup {
         -- your configuration comes here
         -- or leave it empty to use the default settings
         -- refer to the configuration section below
       }
     end
   }
-
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if PACKER_BOOTSTRAP then
-    require('packer').sync()
-  end
-end)
+})
