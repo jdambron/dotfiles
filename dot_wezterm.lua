19b4f6de94a25d7d48d88057e758345961ee741b
Julien Dambron <dreamlike3d@gmail.com>
2023-11-29 13:29:34 +0100

    feat: replace Fira Code with Monaspace

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -13,10 +13,17 @@ end
 -- This is where you actually apply your config choices
 
 config.color_scheme = 'Catppuccin Frappe'
-config.default_prog = { "/bin/bash" }
+config.font = wezterm.font("Monaspace Argon", { weight = "Regular" })
+config.font_rules = {
+  {
+    italic = true,
+    font = wezterm.font("Monaspace Radon", { weight = "Medium" }),
+  },
+}
 config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
 config.window_background_opacity = 0.8
+config.harfbuzz_features = { "calt", "dlig", "clig=1", "ss01", "ss02", "ss03", "ss04", "ss05", "ss06", "ss07", "ss08" }
 
 -- and finally, return the configuration to wezterm
 return config
