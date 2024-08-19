da101479c13ef2b4ed04185923ae8e594a125613
Julien Dambron <dreamlike3d@gmail.com>
2024-08-19 08:50:34 +0200

    feat: update Monaspace configuration

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -4,6 +4,41 @@ local wezterm = require("wezterm")
 -- This will hold the configuration
 local config = wezterm.config_builder()
 
+config.font = wezterm.font({ -- Normal text
+	family = "Monaspace Neon",
+	harfbuzz_features = { "calt", "liga", "dlig", "ss01", "ss02", "ss03", "ss04", "ss05", "ss06", "ss07", "ss08" },
+})
+
+config.font_rules = {
+	{ -- Italic
+		intensity = "Normal",
+		italic = true,
+		font = wezterm.font({
+			family = "Monaspace Radon",
+			style = "Italic",
+		}),
+	},
+
+	{ -- Bold
+		intensity = "Bold",
+		italic = false,
+		font = wezterm.font({
+			family = "Monaspace Krypton",
+			weight = "Bold",
+		}),
+	},
+
+	{ -- Bold Italic
+		intensity = "Bold",
+		italic = true,
+		font = wezterm.font({
+			family = "Monaspace Xenon",
+			style = "Italic",
+			weight = "Bold",
+		}),
+	},
+}
+
 config.color_scheme = "Catppuccin Frappe"
 
 config.enable_tab_bar = false
