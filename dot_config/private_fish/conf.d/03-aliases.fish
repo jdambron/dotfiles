75315531ee46c5c230828a985e4267ffdaa5b9ad
Julien Dambron <dreamlike3d@gmail.com>
2025-04-12 07:31:43 +0200

    feat: use atuin to sync aliases

--- dot_config/private_fish/conf.d/03-aliases.fish
+++
@@ -1,6 +0,0 @@
-# eza instead of ls
-alias ls "eza --color=always --icons --group-directories-first"
-alias la 'eza --color=always --icons --group-directories-first --all'
-alias ll 'eza --color=always --icons --group-directories-first --all --long'
-# editor
-alias e $EDITOR
